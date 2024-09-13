01da8b26611ede7492e49997d6dd5da31a4f2c48
unknown <carlosmviegas86@gmail.com>
2024-09-13 21:05:15 +0100

    created route to sign up user

---
+++ src/routes/users.controller.js
@@ -0,0 +1,15 @@
+
+
+const signup = async(req, res) => {
+  
+  const {  } = req.body;
+  
+  const newUser = {
+    
+  };
+
+};
+
+module.exports = {
+  signup
+};
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const { signup } = require('./users.controller');
+
 const usersRouter = express.Router();
+
+usersRouter.post('/signup', signup);
 
 module.exports = usersRouter;
