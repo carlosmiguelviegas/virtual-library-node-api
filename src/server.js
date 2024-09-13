cbe07138a8f81cbbcfd27f92169817118efd7d76
unknown <carlosmviegas86@gmail.com>
2024-09-13 23:21:58 +0100

    connected the node app to the remote mongo db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const http = require('http');
+const dotenv = require('dotenv');
+
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const database = process.env.DATABASE.replace('<db_password>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(database, {}).then(() => console.log('Database has successfully connected!'));
 
 const PORT = process.env.PORT || 8000;
 
