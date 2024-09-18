d6430b08d4a83c9d39a35e2ade778a3e5135d531
unknown <carlosmviegas86@gmail.com>
2024-09-18 16:57:54 +0100

    fixed conflict

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
-const { signup, login } = require('./../controllers/auth.controller');
+const { signup, login, loggedInGuard, restrictTo } = require('./../controllers/auth.controller');
 
 const usersRouter = express.Router();
 
 usersRouter.post('/signup', signup);
 
 usersRouter.post('/login', login);
+
+usersRouter.route('/').get(loggedInGuard, restrictTo, signup);
 
 module.exports = usersRouter;
