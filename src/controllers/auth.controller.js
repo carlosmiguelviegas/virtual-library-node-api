d8f44261b619c8401ae03f3bb074a4199c40cbd7
unknown <carlosmviegas86@gmail.com>
2024-09-18 15:00:00 +0100

    implemented method to protect routes if user not logged in

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+const promisify = require('promisify')
 const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 
@@ -49,7 +50,35 @@ const login = async(req, res) => {
 
 };
 
+const loggedInGuard = async(req, res, next) => {
+
+  // Check if there´s authorization header and token
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'Please log in to get access.' });
+  }
+
+  // Verification token
+  const verified = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // Check if user still exists
+  const user = await User.findById(verified['id']);
+
+  if (!user) {
+    return res.status(401).json({ message: 'The user with the given token no longer exists.' });
+  }
+
+  // Grant access to protected route
+  next();
+
+};
+
 module.exports = {
   signup,
-  login
+  login,
+  loggedInGuard
 };
