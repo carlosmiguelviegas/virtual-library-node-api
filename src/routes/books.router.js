2bb82b970d8905505dc62ffb83564b0ab07ec290
unknown <carlosmviegas86@gmail.com>
2024-09-21 00:35:08 +0100

    implemented endpoint to rent a book taking into consideration the business logic requirements

--- src/routes/books.router.js
+++ src/routes/books.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { loggedInGuard, restrictTo } = require('./../controllers/auth.controller');
-const { getAllBooks, createNewBook, deleteBook } = require('./../controllers/books.controller');
+const { getAllBooks, createNewBook, deleteBook, rentBook } = require('./../controllers/books.controller');
 
 const booksRouter = express.Router();
 
@@ -10,5 +10,7 @@ booksRouter.get('/', loggedInGuard, getAllBooks);
 booksRouter.post('/', loggedInGuard, restrictTo('admin'), createNewBook);
 
 booksRouter.delete('/:id', loggedInGuard, restrictTo('admin'), deleteBook);
+
+booksRouter.patch('/:id', loggedInGuard, rentBook);
 
 module.exports = booksRouter;
