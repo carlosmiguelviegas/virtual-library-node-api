8841b3a82c571a7da1ab3c254e28ac3565ce37a2
unknown <carlosmviegas86@gmail.com>
2024-10-30 17:41:10 +0000

    installed helmet library

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const cors = require('cors');
+const helmet = require('helmet');
 const apiRouter = require('./routes/api');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 app.use('/api/v1', apiRouter);
 
