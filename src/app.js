6615c8ed7ba4e8c11bac0a6faa5fc4ad10130f55
unknown <carlosmviegas86@gmail.com>
2024-10-31 20:50:48 +0000

    installed compression library for sending responses with less size for optimization

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const cors = require('cors');
 const helmet = require('helmet');
+const compression = require('compression');
 const apiRouter = require('./routes/api');
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
 app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(express.json());
 
 app.use('/api/v1', apiRouter);
 
